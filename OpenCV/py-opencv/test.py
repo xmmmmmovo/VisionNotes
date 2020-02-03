c8500184d11ddc4d19452b555265c907496f60cf
xmmmmmovo <13256172360@163.com>
2020-02-03 14:18:37 +0800

    c

--- OpenCV/py-opencv/test.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-import cv2
-
-g = np.uint8([[[0, 255, 0]]])
-r = np.uint8([[[0, 0, 255]]])
-
-print(cv2.cvtColor(g, cv2.COLOR_RGB2HSV))
-print(cv2.cvtColor(r, cv2.COLOR_RGB2HSV))
