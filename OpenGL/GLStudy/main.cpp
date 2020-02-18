b6db3fe5082cb4dbbbef47d0dcff88780315ab75
xmmmmmovo <13256172360@163.com>
2020-02-18 19:15:47 +0800

    add

---
+++ OpenGL/GLStudy/main.cpp
@@ -0,0 +1,13 @@
+#define GLEW_STATIC
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+int main() {
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+
+    return 0;
+}
